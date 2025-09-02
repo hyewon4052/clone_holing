fdc210a79eb4071b062e47e52f7a44ec5418d40a
hyewon4052 <s2311@e-mirim.hs.kr>
2025-09-02 14:59:50 +0900

    #3 두번 째 섹션 : 동영상 연속 재생 구현 및 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+
+import { useRef, useState } from "react";
 import {
   HeaderContainer,
   Wrapper,
@@ -6,6 +8,7 @@ import {
   DownloadButton,
   LoginButton
 } from "./components/Header.styles";
+
 import {
   IntroductionContainer,
   ContentBox,
@@ -16,11 +19,24 @@ import {
   MainImg,
   CloudBox
 } from "./components/Home.styles";
+
 import {
   BannerContainer,
   BannerImg
 } from "./components/Banner.styles";
 
+import {
+  GoalContainer,
+  TextBox,
+  SubText,
+  MainText,
+  VideoBox,
+  VideoWrapper
+} from "./components/Goal.styles";
+
+import video1 from "./assets/videos/영상_01.mp4";
+import video2 from "./assets/videos/영상_02.mp4";
+import video3 from "./assets/videos/영상_03.mp4";
 import scroll from "./assets/images/main/scroll.svg";
 import bannerBrand from './assets/images/banner/banner_brand.png';
 import bannerInvite from './assets/images/banner/banner_invite.png';
@@ -28,6 +44,18 @@ import logo from "./assets/images/common/favicon.svg";
 
 
 function App() {
+  const videos = [video1, video2, video3];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleEnded = () => {
+    if (currentIndex < videos.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  };
+
 
   return (
     <>
@@ -66,7 +94,32 @@ function App() {
         <BannerImg src={bannerBrand} />
       </BannerContainer>
 
-      
+      <GoalContainer>
+        <TextBox>
+          <SubText>초등부터 중·고등까지</SubText>
+          <MainText style={{ marginBottom: "80px" }}>
+            <span>하루 30분</span>
+            이면 한 학기 수학을 완성할 수 있어요</MainText>
+        </TextBox>
+        <VideoBox>
+          <VideoWrapper>
+            <video
+              ref={videoRef}
+              key={currentIndex}
+              autoPlay
+              onEnded={handleEnded}
+              style={{
+                width: "100%",
+                display: "block",
+                border: "1px solid rgb(191, 197, 203)",
+              }}
+            >
+              <source src={videos[currentIndex]} type="video/mp4" />
+            </video>
+          </VideoWrapper>
+        </VideoBox>
+      </GoalContainer>
+
     </>
   );
 }
