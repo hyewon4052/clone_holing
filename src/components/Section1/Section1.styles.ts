2c41b3286ffee0f718eccf7f0d82e9e88b263f1f
hyewon4052 <s2311@e-mirim.hs.kr>
2025-09-04 11:45:54 +0900

    #4 첫번째 섹션 구름안에 텍스트 띄워지도록 코드 수정

--- src/components/Section1/Section1.styles.ts
+++ src/components/Section1/Section1.styles.ts
@@ -2,79 +2,116 @@ import styled from "styled-components";
 import "../../index.css";
 
 export const IntroductionContainer = styled.div`
-  height: 700px;
+  display: flex;
+  flex-direction: row;
+  gap: 170px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: calc(-415px + 50vh) 0px;
+  position: relative;
+  background-image: url(/assets/images/root/main/bg.png);
   background-repeat: no-repeat;
   background-position: center top;
   background-size: auto;
-  background-position: center;
-  padding: 192px 0px;
-  display: flex;
-  justify-content: center;
-  gap: 170px;
-  width: 100%;
 `;
 
 export const ContentBox = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 340px;
 `;
 
-export const Subtitle = styled.p`
+export const Subtitle = styled.span`
+  font-family: "Suit-semibold";
   font-size: 30px;
-  font-weight: 500;
-  margin-bottom: 12px;
-  color: #000000;
+  font-weight: 700;
+  color: black;
+  text-align: inherit;
+  width: unset;
+  white-space: normal;
+  line-height: 1.5;
 `;
 
 export const Title = styled.h1`
-  font-family: "MapleStory";
+  font-family: "Maplestory";
   font-size: 90px;
   font-weight: 400;
-  margin: 0 0 20px 0;
-  color: #000000;
+  color: black;
+  text-align: inherit;
+  width: unset;
+  white-space: normal;
+  line-height: inherit;
+  -webkit-text-stroke: 2px black;
 `;
 
-export const Description = styled.p`
+export const Description = styled.span`
+  font-family: "Suit-regular";
   font-size: 24px;
-  line-height: 1.5;
-  margin: 28px 0px 0px;
-  color: #757e85;
+  font-weight: 500;
+  color: rgb(117, 126, 133);
+  text-align: center;
+  width: unset;
+  white-space: normal;
+  line-height: 1.6;
 `;
 
 export const CTAButton = styled.button`
+  display: flex;
   width: 320px;
   height: 88px;
-  background-color: #00b300;
-  color: #fff;
-  margin: 80px 0 0;
-  border: none;
-  border-radius: 999px;
-  font-size: 26px;
-  font-weight: 700;
-  cursor: pointer;
-  box-shadow: rgba(0, 0, 0, 0.12) 0px -12px 0px 0px inset;
-  display: flex;
+  padding-top: 25px;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  gap: 8.723px;
+  border-radius: 44px;
+  background: #00ab15;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px -12px 0px 0px inset;
+  cursor: pointer;
+  user-select: none;
+  color: rgb(255, 255, 255);
+  font-family: "Maplestory";
+  font-size: 26px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 0.52px;
+  transition: transform 0.2s ease-in-out;
 `;
 
 export const MainImg = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 export const CloudBox = styled.div`
-  position: absolute;
-  z-index: 10;
   display: flex;
   flex-direction: row;
   gap: 8px;
   align-items: center;
+  background-image: url(/assets/images/root/main/cloud.svg);
   background-repeat: no-repeat;
   background-size: contain;
   background-position: center center;
   width: 436px;
   height: 114px;
   padding: 35px 28px 35px 49px;
+`;
+
+export const NoticeText = styled.span`
+  font-size: 16px;
+  font-weight: 300;
+  color: rgb(74, 78, 83);
+  text-align: center;
+  width: unset;
+  white-space: normal;
+`;
+
+export const HighlightText = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: rgb(0, 171, 21);
+  text-align: center;
+  width: unset;
+  white-space: normal;
 `;
