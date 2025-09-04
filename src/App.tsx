f038356eaba65140e1e612a0b97266a5425f84f3
hyewon4052 <s2311@e-mirim.hs.kr>
2025-09-04 11:48:34 +0900

    #20 footer 구현

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Section5 from "./components/Section5/Section5";
 import Section6 from "./components/Section6/Section6";
 import Section7 from "./components/Section7/Section7";
 import Section8 from "./components/Section8/Section8";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Section6 />
       <Section7 />
       <Section8 />
+      <Footer />
     </>
   );
 }
