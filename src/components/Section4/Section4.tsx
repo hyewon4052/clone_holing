3a1c35fcae60211b8f6c29de46e5c76411cd0ef2
hyewon4052 <s2311@e-mirim.hs.kr>
2025-09-05 17:08:40 +0900

    데이터 배열로 값 넣어서 반환

--- src/components/Section4/Section4.tsx
+++ src/components/Section4/Section4.tsx
@@ -2,6 +2,17 @@ import * as S from "./Section4.styles";
 import { TextBox, MainText } from "../Section2/Section2.styles";
 
 function Section4() {
+  const reasons = [
+    {
+      title: "30억 누적 데이터가 안내하는\n학습 경로로 더 빠르게",
+      img: "assets/images/root/reason/reason1.svg",
+    },
+    {
+      title: "학교 현장에서 인정받는\n학습 내용으로 더 촘촘하게",
+      img: "assets/images/root/reason/reason2.svg",
+    },
+  ];
+
   return (
     <S.ReasonContainer>
       <TextBox>
@@ -12,30 +23,14 @@ function Section4() {
         </MainText>
       </TextBox>
       <S.ReasonBox>
-        <S.ReasonContentBox>
-          <S.ReasonSpan>
-            30억 누적 데이터가 안내하는
-            <br />
-            학습 경로로 더 빠르게
-          </S.ReasonSpan>
-          <img
-            src={
-              import.meta.env.BASE_URL + "assets/images/root/reason/reason1.svg"
-            }
-          />
-        </S.ReasonContentBox>
-        <S.ReasonContentBox>
-          <S.ReasonSpan>
-            학교 현장에서 인정받는
-            <br />
-            학습 내용으로 더 촘촘하게
-          </S.ReasonSpan>
-          <img
-            src={
-              import.meta.env.BASE_URL + "assets/images/root/reason/reason2.svg"
-            }
-          />
-        </S.ReasonContentBox>
+        {reasons.map((r) => {
+          return (
+            <S.ReasonContentBox>
+              <S.ReasonSpan>{r.title}</S.ReasonSpan>
+              <img src={r.img} />
+            </S.ReasonContentBox>
+          );
+        })}
       </S.ReasonBox>
     </S.ReasonContainer>
   );
